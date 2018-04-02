bd49c35394abf0c27f025139b830717d6ed8d42d
sniklaus <simon.niklaus@outlook.com>
2018-04-02 02:44:06 -0700

    no message

--- socket/3-chat.js
+++ socket/3-chat.js
@@ -10,13 +10,9 @@ server.use('/', express.static(__dirname + '/'));
 
 var io = socket(server.listen(process.env.PORT || 8080)); // do not change this line
 
-var objectClients = {};
-
 io.on('connection', function(objectSocket) {
-	// assign a random id to the socket and store the objectSocket in the objectClients variable - example: '9T1P4pUQ'
-	// send the new client the 'hello' event, containing the assigned id - example: { 'strIdent':'9T1P4pUQ' }
-	// send everyone the 'clients' event, contianing an array of the connected clients - example: { 'objectClients':['GxwYr9Uz','9T1P4pUQ'] }
-	// send everyone the 'message' event, containing a message that a new client connected - example: { 'strFrom':'server', 'strTo':'everyone', 'strMessage':'9T1P4pUQ connected' }
+	// send everyone the 'clients' event, contianing an array with the ids of the connected clients - example: { 'strClients':['GxwYr9Uz...','9T1P4pUQ...'] }
+	// send everyone the 'message' event, containing a message that a new client connected - example: { 'strFrom':'server', 'strTo':'everyone', 'strMessage':'9T1P4pUQ... connected' }
 
 	objectSocket.on('message', function(objectData) {
 		// if the message should be recevied by everyone, broadcast it accordingly
@@ -24,9 +20,8 @@ io.on('connection', function(objectSocket) {
 	});
 
 	objectSocket.on('disconnect', function() {
-		// remove the disconnected client from the objectClients variable
-		// send everyone the 'clients' event, contianing an array of the connected clients - example: { 'objectClients':['GxwYr9Uz'] }
-		// send everyone the 'message' event, containing a message that an existing client disconnected - example: { 'strFrom':'server', 'strTo':'everyone', 'strMessage':'9T1P4pUQ disconnected' }
+		// send everyone the 'clients' event, contianing an array of the connected clients - example: { 'strClients':['GxwYr9Uz...'] }
+		// send everyone the 'message' event, containing a message that an existing client disconnected - example: { 'strFrom':'server', 'strTo':'everyone', 'strMessage':'9T1P4pUQ... disconnected' }
 	});
 });
 
