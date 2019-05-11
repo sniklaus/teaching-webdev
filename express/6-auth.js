d680304f1978b1de1e17718461a7d200d4860ceb
Tegan Lamoureux <tegan@teux.me>
2019-05-11 16:47:50 -0700

    Typo and grammar fix.

--- express/6-auth.js
+++ express/6-auth.js
@@ -10,6 +10,6 @@ var strategy = require('passport-http'); // do not change this line
 
 // http://localhost:8080/hello should return 'accessible to everyone' in plain text
 
-// http://localhost:8080/world should return 'only accessible when logged in' in plain text if user the user is authenticate, otherwise will prompt for the username and password
+// http://localhost:8080/world should return 'only accessible when logged in' in plain text if the user is authenticated, otherwise it should prompt for the username and password
 
-// http://localhost:8080/test should return 'only accessible when logged in' in plain text if user the user is authenticate, otherwise will prompt for the username and password
+// http://localhost:8080/test should return 'only accessible when logged in' in plain text if the user is authenticated, otherwise it should prompt for the username and password
